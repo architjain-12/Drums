018128ef982c6fb81214ef0e254b7869536cdd2e
Archit Jain <>
2024-03-08 14:54:59 +0530

    Adds event listener for keydown and mouse click

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let buttonList = document.querySelectorAll("button");
-// console.log(buttonList);
+console.log(buttonList);
 let soundsPath = '/sounds/';
 let soundsName = {'w' : 'crash.mp3', 
                     'a' : 'kick-bass.mp3', 
@@ -7,14 +7,21 @@ let soundsName = {'w' : 'crash.mp3',
                     'd' : 'tom-1.mp3', 
                     'j' : 'tom-2.mp3', 
                     'k' : 'tom-3.mp3', 
-                    'l' : 'tom-4.mp3'}
+                    'l' : 'tom-4.mp3'
+                };
+
 for(i=0; i<buttonList.length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("keypress", function (e) {
-        console.log(e.key);
-        let selectedDrum = e.key;
-        if(Object.keys(soundsName).includes(e.key)) {
-            let audio = new Audio(soundsPath+soundsName[selectedDrum]);
-            audio.play();
-        }
+    document.querySelectorAll(".drum")[i].addEventListener('click', function () {
+        let selectedDrum = this.innerHTML;
+        let audio = new Audio(soundsPath+soundsName[selectedDrum]);
+        audio.play();
     });
 }
+
+addEventListener("keydown", function (e) {
+    let selectedDrum = e.key;
+    if(Object.keys(soundsName).includes(e.key)) {
+        let audio = new Audio(soundsPath+soundsName[selectedDrum]);
+        audio.play();
+    }
+});
